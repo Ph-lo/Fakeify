3ce0ccfe1a254655bbe7b18ed309b7c3d025029a
Philippe Locatelli <philippe.locatelli@epitech.eu>
2023-01-01 21:38:48 +0100

    Server: random date base

---
+++ server/src/models/dates.ts
@@ -0,0 +1,7 @@
+const randomDate = () => {
+  const maxDate = Date.now();
+  const timestamp = Math.floor(Math.random() * maxDate);
+  return new Date(timestamp);
+};
+
+module.exports.getRandomDate = (format: string) => {};
